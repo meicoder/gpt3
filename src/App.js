24358d29195495e18218840cf7ba0712f74fb1c1
Marcos Espinoza <42777227+meicoder@users.noreply.github.com>
2022-07-25 23:02:34 -0400

    Folder structure (#1)
    * components created
    
    * containers created | import containers in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
 const App = () => {
     return (
-        <div>
-            <h1>GPT-3</h1>
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
         </div>
     );
 };
